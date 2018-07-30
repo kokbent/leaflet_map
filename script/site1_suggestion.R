a918078061132bb915b07370bd342b2608cc3a00
kokbent <kokbent@ufl.edu>
2018-07-30 14:10:46 -0400

    Site specific plotly update.

--- script/site1_suggestion.R
+++ script/site1_suggestion.R
@@ -12,13 +12,14 @@ source("script/dailyFunctions.R")
 startDate <- ymd_hms("2017-08-01 00:00:00")
 endDate <- ymd_hms("2018-07-30 23:00:00")
 
-# Sensor water quality carpentry
+#### Data carpentry ####
+## Sensor water quality carpentry
 wq <- read.csv("data/wq.csv", header= T) %>%
-  filter(Site == 1) %>%
+  filter(Site == 2) %>%
   select(Date, Temperature, Salinity)
 wq$Date <- wq$Date %>% ymd_hms %>% round_date(unit="hour")
 
-# Water discharge carpentry (dynamically updating local discharge file)
+## Water discharge carpentry (dynamically updating local discharge file)
 station = '02323500' 
 stinfo  = siteInfo(station)
 dis <- read_rds("data/dis.rds")
@@ -33,28 +34,46 @@ if (max(dis$dates) < (Sys.Date() - 5)) {
 dis2 <- dis %>%
   filter(dates <= date(endDate) & dates >= date(startDate))
 
-# Wind data carpentry
+## Wind data carpentry
+# Run once to build original wind.rds
+# wind17 <- buoy(dataset='cwind',buoyid='CDRF1', datatype='c', year=2017)
+# wind18 <- buoy(dataset='cwind',buoyid='CDRF1', datatype='c', year=2018)
+# wind <- rbind(wind17$data, wind18$data) %>% dplyr::distinct()
+# write_rds(wind, "data/wind.rds")
+
+# Dynamically update wind table, if the endDate is 30 days away from the last date of
+# our existing wind data, the code will pull the new data from NOAA and update our local
+# data
 d <- seq(startDate, endDate, by = "hour")
 yr <- year(d) %>% unique
-wind <- read_rds("data/wind.rds") %>% select(time, WindSpeed=wind_spd)
-if (max(wind$time) < endDate) {
-  # Update
+wind <- read_rds("data/wind.rds")
+last.date <- max(wind$time) %>% date
+if (last.date < date(endDate) - 30) {
+  # In case the local wind.rds hasn't been updated for more than a year
+  yr.missing <- seq(last.date, date(endDate), by = "day")
+  yr.missing <- year(yr.missing) %>% unique
+  for (yr in yr.missing) {
+    wind.new <- buoy(dataset='cwind',buoyid='CDRF1', datatype='c', year=yr)
+    wind <- rbind(wind, wind.new$data) %>% dplyr::distinct()
+  }
+  write_rds(wind, "data/wind.rds")  
 }
-# wind <- buoy(dataset='cwind',buoyid='CDRF1', datatype='c', year=yr)
 
-# Create sequence of dates (so that NA is recognized by geom_line) and build a table of
-# salinity and temperature (remove other irrelevant ones)
+# Shrink the wind table, and convert the format of time
+wind$time <- ymd_hms(wind$time)
+wind <- wind %>%
+  filter(time >= startDate & time <= endDate) %>%
+  select(time, WindSpeed = wind_spd)
+
+## Create daily summary table for all four measure
+# Create a new table of dates that has sequence of datetime between startDate & endDate
+# spaced by one hour, and then join with the wq and wind table
 stw <- data.frame(Date=d) %>%
   left_join(wq, by=c("Date" = "Date")) %>%
   left_join(wind, by=c("Date" = "time"))
 stw$d2 <- date(stw$Date)
 
-# Calculate mean, min and max daily Sal and Temp
-# Note: tidyr's gather is in use here, by converting the "wide" table to
-# "long" one, we can use the aes(color=Measure) in ggplot
-# Note2: I've created functions in dailyFunction.R to make calculating daily mean, min,
-# max easy. The function allows certain proportions of NA here. In this example, 0.75
-# of the daily numbers can be NA, and we'll still calculate the meant, min, max
+# Calculate mean, min and max daily Sal, Temp and wind speed
 stw_summ <- stw %>%
   gather(key = "Measure", value = "Value", Salinity, Temperature, WindSpeed) %>%
   group_by(d2, Measure) %>%
@@ -68,66 +87,55 @@ dis2 <- dis %>%
   mutate(Datetime = paste(dates, " 12:00:00") %>% ymd_hms()) %>%
   filter(Datetime >= startDate & Datetime <= endDate)
 
+# Since water discharge doesn't have hourly value, we create a table that looks like
+# stw_summ so we can bind the water discharge information to the stw_summ
+# In this case we only have the meanVal
 dis3 <- dis2 %>%
   mutate(meanVal=val/1000) %>%
   select(d2=dates, meanVal) %>%
   mutate(Measure = "Discharge")
 
 stwd_summ <- bind_rows(stw_summ, dis3)
-st_summ <- stwd_summ %>% filter(Measure %in% c("Temperature", "Salinity"))
-wd_summ <- stwd_summ %>% filter(Measure %in% c("WindSpeed", "Discharge"))
 
-p1 <- ggplot() +
-  geom_line(data=st_summ, aes(x=d2, y=meanVal, color=Measure)) +
-  geom_ribbon(data=st_summ, aes(x=d2, ymax=maxVal, ymin=minVal, fill=Measure),
-              alpha=0.5)
+#### Plotting using plotly ####
+# Color chart is to define what color you want for each Measure, in this case black for
+# Discharge, blue for Salinity, Orange for Temperature and Pink for windspeed
+colorChart <- data.frame(Measure = c("Discharge", "Salinity", "Temperature", "WindSpeed"),
+                         color = c('rgba(0, 0, 0, 0.8)', 'rgba(0, 114, 178, 0.8)',
+                                   'rgba(213, 94, 0, 0.8)', 'rgba(204, 121, 167, 0.8)'))
 
-p1
-
-p2 <- ggplot() +
-  geom_line(data=wd_summ, aes(x=d2, y=meanVal, color=Measure)) +
-  geom_ribbon(data=wd_summ, aes(x=d2, ymax=maxVal, ymin=minVal, fill=Measure),
-              alpha=0.5)
-p2
-
+# This part is a bit more complicated...
+# The function is to create a plotly plot for one of the measure.
+# Each of the plotly plot has a line (for daily mean), and a ribbon (for min and max)
+# The function also check the colorChart to assign the color associated with the measure
 plot4 <- function(df) {
   name <- df$Measure %>% unique
+  col <- colorChart$color[colorChart$Measure == name]
   p <- plot_ly(data = df, x = ~d2, y = ~meanVal) %>%
-    add_lines(name = name) %>%
-    add_ribbons(ymin = ~minVal, ymax = ~maxVal)
+    add_lines(name = name, 
+              line = list(color = col),
+              hoverinfo = 'text',
+              text = ~paste("Date :", d2, 
+                            "</br></br> Mean", Measure, ":", round(meanVal, 2),
+                            "</br> Min : ", round(minVal, 2), "Max :", round(maxVal, 2))) %>%
+    add_ribbons(ymin = ~minVal, ymax = ~maxVal, 
+                line = list(color = 'rgba(255, 255, 255, 0)'),
+                fillcolor = col,
+                opacity = 0.5,
+                showlegend = F,
+                hoverinfo = 'none')
   return(p)
 }
 
-k <- stwd_summ %>%
+# Take stwd_summ, based on the Measure, split the table into a list of four table
+# so you get a list of (say k), then k$Discharge or k[[1]] will be a table with only Discharge
+# information. Then using lapply function, it basically says, for each element of the list,
+# apply the plot4 function. So after the lapply step, we get a list of four plotly plots.
+# Finally, using the plotly function subplot, we arrange the four plots into one column, aligning
+# according to the xaxis ('shareX = T')
+combine.plot <- stwd_summ %>%
   split(stwd_summ$Measure) %>%
   lapply(plot4) %>%
   subplot(nrows = 4, shareX = T)
 
-p <- stwd_summ %>% filter(Measure != "Discharge") %>%
-  ggplot() +
-  geom_line(aes(x=d2, y=meanVal, color=Measure)) +
-  geom_ribbon(aes(x=d2, ymin=minVal, ymax=maxVal), fill="grey", alpha=0.3) +
-  facet_wrap(~ Measure, ncol = 1)
-
-p <- ggplotly()
-p
-# Second step is to specify the scaling of x and y axes, by using the breaks argument
-# we can control what to display on the y main and secondary axes, e.g. 0 to 40 for main,
-# and 0 to 20000 for secondary
-p <- p + 
-  scale_y_continuous(name = "Temperature / Salinity (deg C / ppt)", 
-                     limits=c(-20,40), 
-                     breaks = seq(0, 40, 10),
-                     sec.axis = sec_axis(~(.+20), 
-                                         name = "River Discharge ('000 cfs)",
-                                         breaks = seq(0, 20, 10))) +
-  scale_x_date(date_breaks = "2 month", date_labels = "%Y/%m", expand = c(0, 0))
-
-# Final step is to specify the theme
-p <- p + theme(legend.position=("top"),
-          panel.border = element_rect(color = "black", size = 1, fill = NA, linetype="solid"),
-          axis.text=element_text(size=10),
-          axis.title=element_text(size=13,face="bold"),
-          plot.title =element_text(size=13, face='bold'),
-          axis.text.x = element_text(angle = 45, hjust = 1))
-ggplotly(p)
+combine.plot
